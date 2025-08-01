e413ec682b86a6e4673874e9dfbc4a2955874390
Doug Stensrud <dwsappend@gmail.com>
2025-08-01 04:38:40 -0700

    oops, now it passes all tests

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function confirmEnding(string, ending) {
   let strLength = string.length;
-  let endsWith = string.slice(strLength - 1);
+  let endLength = ending.length;
+  let endsWith = string.slice(strLength - endLength);
   return endsWith === ending ? true : false;
-  console.log(endsWith);
 }
 
 let string = "Bastian";
-let ending = "n";
+let ending = "an";
 console.log(confirmEnding(string, ending));
